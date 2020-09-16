776473f98a34968dc09d41fbc85b1444a5bab7b9
woong97 <yjwoong97@gmail.com>
2020-09-17 03:36:41 +0900

    Make Run File

---
+++ main.py
@@ -0,0 +1,101 @@
+# Some code was borrowed from https://github.com/lyeoni/pytorch-mnist-VAE/blob/master/pytorch-mnist-VAE.ipynb 
+
+from model import *
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import os
+
+train_dataset = datasets.MNIST(
+                    root = './mnist_data/',
+                    train = True,
+                    transform=transforms.ToTensor(),
+                    download = True
+                )
+
+test_dataset = datasets.MNIST(
+                    root = './mnist_data/',
+                    train = False,
+                    transform=transforms.ToTensor(),
+                    download = False
+                )
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size = 128, shuffle = True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size = 128, shuffle = False)
+
+if torch.cuda.is_available():
+    device = torch.device('cuda:0')
+    torch.cuda.empty_cache()
+else:
+    device = torch.device('cpu')
+
+
+def experiment(epochs):
+
+    model = VAE(img_dim = 784, z_dim = 20, hidden_dim = 512, dropout = 0.5).to(device)
+    optimizer = optim.Adam(model.parameters(), lr = 1e-04)
+
+    for epoch in range(epochs):
+        model.train()
+        train_loss = 0
+
+        for _, (x, _) in enumerate(train_loader):
+            x = x.to(device)
+
+            x_target, mu, sigma = model(x.view(-1,784))
+
+            loss = vae_loss(x_target, x.view(-1,784), mu, sigma)
+            train_loss += loss.item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        train_loss = train_loss / len(train_loader)
+
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for _, (x_test, _) in enumerate(test_loader):
+                x_test = x_test.to(device)
+
+                x_target_test , mu_test, sigma_test = model(x_test.view(-1,784))
+
+                loss_test = vae_loss(x_target_test, x_test.view(-1,784), mu_test, sigma_test)
+                test_loss += loss_t.item()
+
+            test_loss = test_loss / len(test_loader)
+
+        print('===== epoch:{}. Training Loss: {:.3f} / Test Loss : {:.3f}. '
+                                                .format(epoch, train_loss, test_loss))
+
+    path = os.path.join('model_pt', 'vae.pt')
+    torch.save(model.state_dict(), path)
+    print("Trained model saved")    
+
+
+def generate_img():
+    
+    model = VAE(img_dim = 784, z_dim = 20, hidden_dim = 512, dropout = 0.5).to(device)
+    state_dict = torch.load(os.path.join('model_pt', 'vae.pt'), map_location = device)
+    model.load_state_dict(state_dict, strict = False)
+    
+    model.eval()
+    with torch.no_grad():
+        latent_z = torch.randn(64,20).to(device)
+        sample = model.decoder(latent_z).to(device)
+
+        save_image(sample.view(64, 1, 28, 28), './samples/sample_1.png')
+
+
+
+if __name__ == '__main__' :
+
+    # experiment(epochs = 20)
+    generate_img()
+    
+
+
--- model.py
+++ model.py
@@ -1,29 +1,31 @@
+# Some code was borrowed from https://github.com/hwalsuklee/tensorflow-mnist-VAE/blob/master/vae.py
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, dropout = 0.5):
+    def __init__(self, img_dim, hidden_dim, z_dim, dropout = 0.5):
         super(Encoder, self).__init__()
         
-        self.input_dim = input_dim
+        self.img_dim = img_dim
         self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
+        self.z_dim = z_dim
 
         self.drop = nn.Dropout(p = dropout)
 
         self.linear1 = nn.Linear(
-                            self.input_dim,
+                            self.img_dim,
                             self.hidden_dim
                          )
         self.linear2 = nn.Linear(
                             self.hidden_dim,
                             self.hidden_dim,
                         )
-        # In order to make mean, stddev, we take output dimension equals 2 * self.output_dim 
+        # In order to make mean, stddev, we take output dimension equals 2 * self.z_dim 
         self.out_layer = nn.Linear(
                             self.hidden_dim,
-                            self.output_dim * 2
+                            self.z_dim * 2
                         )
 
         # He Initialization
@@ -44,22 +46,22 @@ class Encoder(nn.Module):
         params = self.out_layer(x)
 
         # mu : mean, sigma : standard deviation
-        mu = params[:, :self.output_dim]
+        mu = params[:, :self.z_dim]
         # The standard deviation must be positive. Parametrize with a softplus and
         # add a small epsilon for numerical stability
-        sigma = 1e-6 + F.softplus(params[:, self.output_dim:])
+        sigma = 1e-6 + F.softplus(params[:, self.z_dim:])
 
         return mu, sigma
 
 
 class Decoder(nn.Module):
 
-    def __init__(self, z_dim, hidden_dim, output_dim, dropout = 0.5):
+    def __init__(self, z_dim, hidden_dim, img_dim, dropout = 0.5):
         super(Decoder, self).__init__()
 
         self.z_dim = z_dim
         self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
+        self.img_dim = img_dim
         
         self.drop = nn.Dropout(p = dropout)
 
@@ -73,7 +75,7 @@ class Decoder(nn.Module):
                         )
         self.linear3 = nn.Linear(
                             self.hidden_dim,
-                            self.output_dim
+                            self.img_dim
                         )
 
         # He Initialization
@@ -92,17 +94,15 @@ class Decoder(nn.Module):
 
         x = torch.sigmoid(self.linear3(x))
 
-        return x 
+        return x
 
 
 # In order to use backpropagation, reparametrize sampling 
 class VAE(nn.Module):
     
-    def __init__(self, x_hat, x_target, img_dim, z_dim, hidden_dim, dropout):
+    def __init__(self, img_dim, z_dim, hidden_dim, dropout):
         super().__init__()
 
-        self.x_hat = x_hat
-        self.x_target = x_target
         
         self.img_dim = img_dim
         self.z_dim = z_dim
@@ -110,60 +110,51 @@ class VAE(nn.Module):
         
         self.dropout = dropout
 
-    def loss(self):
-
-        batch_size = self.x_hat.size(0)
-        
-        # sampling by re-parametrization in order to use back-propagation
-        encoder = Encoder(self.img_dim, self.hidden_dim, self.img_dim, self.dropout)
-        mu, sigma = encoder(self.x_hat)
-        z = mu + sigma * torch.randn(mu.shape)
-
-        decoder = Decoder(self.img_dim, self.hidden_dim, self.img_dim, self.dropout)
-        y = decoder(z)
-        y = torch.clamp(y, 1e-8, 1 - 1e-8)
-
-        # Loss = reconstruction error + KL divergence loss
-        generative_loss = F.binary_cross_entropy(y, self.x_target)
-
-        KLD_loss = 0.5 * torch.sum(
-                                    torch.pow(mu, 2) +
-                                    torch.pow(sigma, 2) -
-                                    torch.log(1e-8 + torch.pow(sigma, 2)) -1
-                                ).sum() / batch_size
-        
-        loss = generative_loss + KLD_loss
-        
-        return loss
+        self.encoder = Encoder(
+                            self.img_dim,
+                            self.hidden_dim,
+                            self.z_dim,
+                            self.dropout
+                        )
+        self.decoder = Decoder(
+                            self.z_dim,
+                            self.hidden_dim,
+                            self.img_dim,
+                            self.dropout
+                        )
 
         
-    def get_ae(self, x):
+    def forward(self, x):
 
-        encoder = Encoder(x.shape[1], self.hidden_dim, self.z_dim, self.dropout)
-        mu, sigma = encoder(x)
+        mu, sigma = self.encoder(x)
+        z = mu + sigma * torch.randn_like(mu)
+        x_target = self.decoder(z)
 
-        z = mu + sigma * torch.randn(mu.shape)
-
-        decoder = Decoder(self.z_dim , self.hidden_dim, self.img_dim, 1.0)
-        y = decoder(z)
-        return y
+        return x_target, mu, sigma
 
 
-
-
+def vae_loss(x_target, x, mu, sigma):
+    batch_size = x.size(0)
     
+    generative_loss = F.binary_cross_entropy(x_target, x)
+
+    KLD_loss = 0.5 * torch.sum(
+                                torch.pow(mu, 2) +
+                                torch.pow(sigma, 2) -
+                                torch.log(1e-8 + torch.pow(sigma, 2)) -1
+                            ).sum() / batch_size
+    
+    loss = generative_loss + KLD_loss
+    
+    return loss
+
+
+
 
 # a = torch.rand(10, 30)
 # b = torch.rand(10, 30)
 # c = torch.rand(10, 30)
-
-# model = VAE(a,b, 20, 40, 40, 0.5)
+# model = VAE(a,b, 30, 40, 40, 0.4)
 # model.eval()
 # print(model.get_ae(c))
-
-
-
-
-
-
 
